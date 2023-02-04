6bad7e7cd5ce30142a0de7fac30d5166af70f234
Jack'lul <jacklulcat@gmail.com>
2023-02-04 19:27:10 +0100

    Do not use dos2unix on phar...

--- miap-controller/install.sh
+++ miap-controller/install.sh
@@ -8,8 +8,6 @@ if [ -f "$SPATH/miap-controller.phar" ]; then
 		cp -v $SPATH/miap-controller.phar /usr/local/bin/miap-controller && chmod 755 /usr/local/bin/miap-controller
 		cp -v $SPATH/miap-controller-system.service /etc/systemd/system/miap-controller.service && chmod 644 /etc/systemd/system/miap-controller.service
 		
-		command -v dos2unix >/dev/null 2>&1 && dos2unix /usr/local/bin/miap-controller
-		
 		echo -e "\nTo enable the service use this command: \"sudo systemctl enable miap-controller.service\""
 
 		systemctl daemon-reload
@@ -19,8 +17,6 @@ if [ -f "$SPATH/miap-controller.phar" ]; then
 
 		mkdir -pv ~/.config/systemd/user/
 		cp -v $SPATH/miap-controller-user.service ~/.config/systemd/user/miap-controller.service && chmod 644 ~/.config/systemd/user/miap-controller.service
-		
-		command -v dos2unix >/dev/null 2>&1 && dos2unix ~/.local/bin/miap-controller
 		
 		echo -e "\nTo enable the service use this command: \"systemctl --user enable miap-controller.service\""
 		echo "You might also need to run \"loginctl enable-linger $USER\" to enable the launch of services for not logged in users"
